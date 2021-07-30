d27943b49089f56af25a794486a9e046aede30e2
Ben <bkoenig.as@gmail.com>
2021-07-30 11:22:45 +0200

    added footer

--- source/js/app.js
+++ source/js/app.js
@@ -1,2 +1,10 @@
+import gsap from "gsap";
 
-
+const tl = gsap.timeline({repeat:-1});
+tl.add('text');
+tl.from('#text_1', {x:'-100%'});
+tl.to('#text_1', {x:'100%', delay:1})
+tl.from('#text_2', {x:'-100%'}, ">");
+tl.to('#text_2', {x:'100%', delay:1})
+tl.from('#text_3', {x:'-100%'});
+tl.to('#text_3', {x:'100%', delay:1});
